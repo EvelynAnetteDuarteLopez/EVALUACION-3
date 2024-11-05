2a85a2bf8f3f2ad6c36121e12d0b5eb55dadb2d7
EvelynAnetteDuarteLopez <L24550849@chihuahua2.tecnm.mx>
2024-11-05 17:50:02 -0600

    Add files via upload

---
+++ EVA3_1_METODOS/src/eva3_1_metodos/EVA3_1_METODOS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_metodos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_METODOS {
+    public static void main(String[] args) {
+      //USO DE LOS METODOS:
+        //INVOCACION O LLAMADA
+        imprimirMensaje("Hola mundo!1");
+        
+    }
+    //DECLARACION
+    //encabezado 
+    //IMPRIMIR UN MENSAJE
+    //JAVA: iniciar en minuscula, verbo, siguientes palabras inician en mayuscula
+    public static void imprimirMensaje(String mensaje){
+        System.out.println("TU MENSAJE:" + mensaje);
+        
+    }
+    
+}
